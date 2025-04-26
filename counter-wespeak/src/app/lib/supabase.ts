742a0de35d78ec455750371905c0920734100df0
tomasPerez <tomasperez.henry@gmail.com>
2025-04-26 19:34:05 -0300

    check setted value

--- counter-wespeak/src/app/lib/supabase.ts
+++ counter-wespeak/src/app/lib/supabase.ts
@@ -3,5 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Crear y exportar el cliente de Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
